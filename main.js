967e7f372dc3f260a42c63b0874ac651c1dd754e
Sandro <sandro.sikic@gmail.com>
2022-04-22 16:24:23 +0200

    added import feature, udpated selectors for version and type

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function addField(title, name) {
 
 	const input = document.createElement('input');
 	input.name = name;
+	input.oninput = preview;
 
 	label.appendChild(span);
 	label.appendChild(input);
@@ -37,7 +38,7 @@ function preview() {
 	});
 
 	output.innerHTML = JSON.stringify(
-		{ card_content: baseObject, btn_group: ctaObject },
+		{ card_content: { ...baseObject, btn_group: ctaObject } },
 		null,
 		2
 	);
@@ -50,6 +51,7 @@ function addSelect(title, name, options) {
 
 	const select = document.createElement('select');
 	select.name = name;
+	select.oninput = preview;
 
 	for (const key in options) {
 		const option = document.createElement('option');
@@ -66,7 +68,6 @@ function addSelect(title, name, options) {
 
 function removeCTA(name) {
 	document.getElementById(name).remove();
-	preview();
 }
 
 function addCTA() {
@@ -85,6 +86,7 @@ function addCTA() {
 	remove.onclick = (event) => {
 		event.preventDefault();
 		removeCTA(name);
+		preview();
 	};
 	remove.innerHTML = 'Remove';
 
@@ -93,11 +95,10 @@ function addCTA() {
 	const eventName = addField('Event name: ', 'event_name');
 	const data = addField('Data: ', 'data');
 	const type = addSelect('Type: ', 'type', {
-		website: 'Website',
+		WEBSITE: 'WEBSITE',
 	});
 	const style = addSelect('Style: ', 'style', {
-		'VIEW01.V01': 'View01v01',
-		'VIEW01.V02': 'View01v02',
+		'VIEW01.v01': 'VIEW01.v01',
 	});
 
 	form.appendChild(id);
@@ -113,6 +114,8 @@ function addCTA() {
 
 	cta.appendChild(form);
 	preview();
+
+	return form;
 }
 
 function registerOnChangeEvent(tag) {
@@ -121,14 +124,6 @@ function registerOnChangeEvent(tag) {
 		element.oninput = preview;
 	}
 }
-
-preview();
-
-setInterval(() => {
-	registerOnChangeEvent('input');
-	registerOnChangeEvent('textarea');
-	registerOnChangeEvent('select');
-}, 1000);
 
 function copyMessage(type) {
 	const button = document.getElementById('copy');
@@ -192,3 +187,74 @@ function copyToClipboard() {
 			copyMessage('error');
 		});
 }
+
+function showImport() {
+	const self = this.event.target;
+
+	var content = self.nextElementSibling;
+
+	if (content.style.maxHeight) {
+		self.innerText = 'Import';
+		content.style.maxHeight = content.scrollHeight + 'px';
+		content.style.overflow = 'hidden';
+		requestAnimationFrame(function () {
+			content.style.maxHeight = null;
+		});
+	} else {
+		self.innerText = 'Close Import';
+		content.style.maxHeight = content.scrollHeight + 'px';
+		setTimeout(() => {
+			content.style.maxHeight = 'unset';
+			content.style.overflow = 'unset';
+		}, 300);
+	}
+
+	const element = document.getElementById('import');
+	element.oninput = parseImport;
+}
+
+function parseImport(element) {
+	try {
+		JSON.parse(element.target.value).card_content;
+		document.getElementById('import-error').style.display = 'none';
+	} catch (error) {
+		document.getElementById('import-error').style.display = 'block';
+		return;
+	}
+
+	const card_content = JSON.parse(element.target.value).card_content;
+
+	for (const key in card_content) {
+		const element = document.querySelector(`[name="${key}"]`);
+		if (element) {
+			element.value = card_content[key];
+		}
+	}
+
+	const cta_ids = [];
+	document.getElementById('cta').childNodes.forEach((cta) => {
+		cta_ids.push(cta.getAttribute('id'));
+	});
+
+	for (const cta_id of cta_ids) {
+		removeCTA(cta_id);
+	}
+
+	console.log('card_content.btn_group', card_content['btn_group']);
+
+	for (let index = 0; index < card_content.btn_group.length; index++) {
+		cta = addCTA();
+		for (const [key, value] of Object.entries(card_content.btn_group[index])) {
+			const item = cta.querySelector(`[name="${key}"]`);
+			if (item) {
+				item.value = value;
+			}
+		}
+	}
+	preview();
+}
+
+registerOnChangeEvent('input');
+registerOnChangeEvent('textarea');
+registerOnChangeEvent('select');
+preview();
