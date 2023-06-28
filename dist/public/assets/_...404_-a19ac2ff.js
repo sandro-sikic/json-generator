25c3b9972867bbe45d37134818ecf8b1e7d394dd
Sandro <sandro.sikic@gmail.com>
2023-06-28 15:13:51 +0200

    build: feature input validation

---
+++ dist/public/assets/_...404_-a19ac2ff.js
@@ -0,0 +1,1 @@
+import{u as a}from"./index-299798ac.js";function e(){a()("/")}export{e as default};
--- dist/public/assets/_...404_-f21c5fa2.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./index-8b277b6f.js";function e(){a()("/")}export{e as default};
