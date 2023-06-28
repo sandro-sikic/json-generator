ae13784e7321b01af673c1966029f826c4f2b63b
Sandro <sandro.sikic@gmail.com>
2023-06-28 15:12:47 +0200

    style: fixed home page font size

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,7 +8,9 @@ export default function Home() {
 	return (
 		<main class="flex flex-col mx-auto flex-grow max-w-sm justify-center items-center gap-5">
 			<Zumiez className="w-full" />
-			<h1 class="w-full text-6xl text-gray-900 font-thin">Json Generator</h1>
+			<h1 class="w-full text-5xl text-gray-900 font-thin whitespace-nowrap">
+				Json Generator
+			</h1>
 			<div className="flex gap-4 justify-center">
 				<Button onClick={() => navigate('/static')}>Static</Button>
 				<Button onClick={() => navigate('/events')}>Events</Button>
